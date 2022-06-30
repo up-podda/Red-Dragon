fa4b27aadccc24f8b4bd0c3fe2dfd01065d61039
up-podda <107332661+up-podda@users.noreply.github.com>
2022-06-30 06:29:53 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,13 +21,13 @@ global.APIKeys = {
 //customize settings\\
 
 global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+My Creator is ༒☬ꪶ࿋྄ིꫂÇhͥαmͣïͫ†h༻☬༒` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94769228163'] //Owner number in aive msg
+global.premium = ['+94769228163'] //Owner Number info
+global.ownernomer = '+94769228163' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
+global.ownername = '༒☬ꪶ࿋྄ིꫂÇhͥαmͣïͫ†h༻☬༒' //Owner Name
 global.botname = ' ☬RED༒DRAGON' //Bot Name
 
 global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
@@ -36,7 +36,7 @@ global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's 
 global.region = 'India, South India, Kerala' //Country Region
 
 global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.author = '༒☬ꪶ࿋྄ིꫂÇhͥαmͣïͫ†h༻☬༒❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor In Menu!
